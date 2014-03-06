c53472fe4775d6cc1d378c2327d8656bb3b651e3
jonnadul <jonnadul@gmail.com>
2014-03-05 23:28:05 -0800

    Added stuff to do firebase

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
-var schoolmeinApp = angular.module('schoolmeinApp', ['ngSanitize']);
+var schoolmeinApp = angular.module('schoolmeinApp', ['ngSanitize'], ['firebase']);
+
+schoolmeinApp.controller('SampleController', ['$scope', '$firebase', '$firebaseSimpleLogin',
+    function($scope, $firebase, $firebaseSimpleLogin) {
+      var ref = new Firebase("https://schoolmeinApp.firebaseio.com/");
+      $scope.auth = $firebaseSimpleLogin(ref);
+    }]);
